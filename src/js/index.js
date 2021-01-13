7da936eb88e0fb3d5ef12cecbf8a1bd7081641d3
plll <pl1506211>
2021-01-13 14:33:26 +0800

    1.基本效果完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-11 17:41:40
- * @LastEditTime: 2021-01-12 16:36:44
+ * @LastEditTime: 2021-01-13 14:31:49
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \canvas\src\js\index.js
@@ -82,29 +82,46 @@ const drawRound = (ctx, rgba, item, r) => {
 
 // 开始表演
 const draw = (ctxCopy,data) => {
-  let r = randomNum(100);
-  // for (let i = 0; i < data.length; i += randomNum(20)) {
-  //   let item = data[i]
-  //   let rgba  = `rgba(${item.r}, ${item.g}, ${item.b}, ${item.a})`;
-
-  //   // drawRect(ctxCopy, rgba, item, r)
-  //   // drawRound(ctxCopy, rgba, item, r)
-  //   // writeText(ctxCopy, rgba, item, r)
-
-  //   curveFormula(item.p0,item.p1,item.p2, {ctxCopy, rgba, item, r})
-  // }
-
-  let i = 0
+  // 圆半径，正方形宽高
+  let r = randomNum(5);
+  let RAF
+  // 17毫秒 执行一次循环 加入事件循环
   const dong = () => {
-    if (i >= data.length) {
-      cancelAnimationFrame(a)
+    let isOver = 0
+
+    ctxCopy.clearRect(0,0,canvasCopy.width,canvasCopy.height);
+
+    for (let k = 0; k < data.length; k += 1) {
+      let item = data[k]
+      let rgba  = `rgba(${item.r}, ${item.g}, ${item.b}, ${item.a})`; 
+      // 延迟发射
+      if (item.curDelay < item.delay) {
+        item.curDelay++
+        continue
+      }
+      // item.t区间值（0,1）
+      if (item.t < 1) {
+        item.t += 0.01
+        let curItem = curveFormula(item.p0,item.p1,item.p2, item.t)
+        drawRound(ctxCopy, rgba, curItem, r)
+        // drawRect(ctxCopy, rgba, curItem, r)
+        // writeText(ctxCopy, rgba, curItem, r)
+      } else {
+        isOver++     
+        drawRound(ctxCopy, rgba, item, r) 
+        // drawRect(ctxCopy, rgba, item, r)
+        // writeText(ctxCopy, rgba, item, r)
+        // console.log(isOver, item)
+      } 
+      // 所以粒子都执行完成
+      if (isOver >= data.length) {
+        console.log('结束', data, isOver, data.length )
+        return cancelAnimationFrame(RAF)
+      }             
     }
-    i += randomNum(20)
-    let item = data[i]
-    let rgba  = `rgba(${item.r}, ${item.g}, ${item.b}, ${item.a})`;   
-    curveFormula(item.p0,item.p1,item.p2, {ctxCopy, rgba, item, r})
-    // drawRound(ctxCopy, rgba, item, r) 
-    let a = requestAnimationFrame(dong)
+   
+    RAF = requestAnimationFrame(dong)
+    
   }
   dong()
 }
@@ -120,12 +137,8 @@ const curvePath = (ctx, p0,p1,p2,color = 'blue') => {
   ctx.stroke()
 }
 
-// 运动物体
-
 // 二阶曲线公式 p0, p1, p2 三个点的坐标
-const curveFormula = (p0, p1, p2, data) => {
-  let {ctxCopy, rgba, item, r} = data
-  let arr = []
+const curveFormula = (p0, p1, p2, t) => {
   // 获取两个点之前的运动轨迹
   const coordinates = (t) => {
     let x = (1-t)*(1-t)*p0.x+2*t*(1-t)*p1.x+t*t*p2.x
@@ -135,26 +148,7 @@ const curveFormula = (p0, p1, p2, data) => {
       y
     }
   }
-
-    let t = 0
-    let a
-  const dong = () => {
-    if (t >= 1) {
-      cancelAnimationFrame(a)
-    }
-    t += 0.01
-    drawRound(ctxCopy, rgba, coordinates(t), r) 
-     a = requestAnimationFrame(dong)
-  }
-  dong()
-  // for (let t = 0;t < 1; t += 0.1) {
-  //   const dong = () => {
-  //     drawRound(ctxCopy, rgba, coordinates(t), r) 
-  //     let a = requestAnimationFrame(dong)
-  //   }
-  //   dong()
-  // }
-  return arr
+  return coordinates(t)
 }
 
 // 创建线程
@@ -176,10 +170,12 @@ const getImgData = (ctx, canvas) => {
   let length = canvas.width * canvas.height;
   const myImage = ctx.getImageData(0, 0, canvas.width, canvas.height)
   let info = []
-  const copyImgData = creatImageData(ctx)
 
-
-  for (let i = 0; i < length * 4; i += 4 * 5) {
+  // 跨几倍像素
+  const multiple = 20
+  // p1 随机数
+  const p1Random = 2 || Math.random()
+  for (let i = 0; i < length * 4; i += 4 * multiple) {
     // 灰度
     let myRed = myImage.data[i];
     let myGreen = myImage.data[i + 1];
@@ -197,8 +193,8 @@ const getImgData = (ctx, canvas) => {
       y,
     };
     let p1 = {
-      x: (p0.x - x ) / 2,
-      y: (p0.y - y) / 2
+      x: (p0.x - x )  / p1Random,
+      y: (p0.y - y)  / p1Random
     };
     // 获取xy，色值
     info.push({
@@ -211,7 +207,9 @@ const getImgData = (ctx, canvas) => {
       p0,
       p2,
       p1,
-      // arr: curveFormula(p0,p1,p2)    
+      t:0,
+      delay: Math.ceil(3000 / 16.66) * Math.random(),
+      curDelay: 0,
     })
 
   }
@@ -238,10 +236,3 @@ img.onload = () => {
 
 addBody(canvas)
 addBody(canvasCopy)
-
-
-// let p0 = {x:0,y:0}
-// let p1 = {x:120,y:100}
-// let p2 = {x:200,y:50}	
-// let arr = curveFormula(p0,p1,p2)
-// console.log(arr, '---')
